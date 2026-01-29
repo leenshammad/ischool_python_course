f4cb822a025344c68be33603ac7873463db09c43
leen <leenhammad2025@gmail.com>
2026-01-29 17:30:56 +0200

    add head or tail

---
+++ heads_or_tails.py
@@ -0,0 +1,11 @@
+# task: predict if the output is heads or tails randomly
+# inputs: n/a
+# output: heads or tails
+
+import random
+
+outcomes= ['heads', 'tails']
+
+result = random.choice(outcomes)
+
+print(result)
