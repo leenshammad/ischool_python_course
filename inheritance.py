115dad64e5bfc2cd4137700901f22de22f46e914
leen <leenhammad2025@gmail.com>
2026-02-13 18:11:21 +0200

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,12 @@
+class Watermelon_slices:
+    def __init__(self,shap,color):
+        self.color=color
+        self.shap=shap
+    def area(self):
+        pass
+class Circular_slice (Watermelon_slices):
+    def __init__(self, shap, color,raiduis):
+        super().__init__("circle", color)
+        self.raiduis=raiduis
+    def area(self):
+        return 3.14*self.raiduis**2
