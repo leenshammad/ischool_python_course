a369eed6c93ae7e793486090fab175eed6628ff6
leen <leenhammad2025@gmail.com>
2026-03-28 14:34:05 +0200

    square

---
+++ turtle_square.py
@@ -0,0 +1,16 @@
+
+import turtle
+
+
+t = turtle.Turtle()
+
+
+for _ in range(4):
+    t.forward(100)
+    t.penup()
+    t.forward(20)
+    t.left(90)
+    t.forward(20)
+    t.pendown()
+
+turtle.done()
