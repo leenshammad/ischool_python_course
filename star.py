127261d1cb675c678cc8444722e24dcaf81b440e
leen <leenhammad2025@gmail.com>
2026-04-10 18:27:09 +0200

    update the file names

--- star.py
+++ star.py
@@ -1,29 +1,30 @@
-import turtle
+import turtle 
 
 t=turtle.Turtle()
-def draw_oxtgon():
-    for i in range (8):
-        t.forward(100)
-        t.left(45)
+# def draw_star(size,point):
+#     for i in range (point):
+   
+#         t.forward(100)
+#         if i%2==0:
+#             t.left(size)
+#         else:
+#             t.left(size)
 
-def draw_circle():
-    t.color(0.8,0.3,0.4)
-    t.begin_fill()
-    t.circle(100)
-    t.end_fill()
+# draw_star(20,15)
+# turtle.done()
 
-def draw_star():
+
+def draw_star(size,point):
     for i in range (1,19):
-        t.forward(100)
+        t.forward(point)
         if i%2==0:
-            t.left(175)
+            t.left(size)
         else:
             t.left(225)
-
-
-draw_oxtgon()
-draw_star()
-draw_circle()
-
+    def draw_star():
+     t.color(0.8,0.3,0.4)
+     t.begin_fill()
+     t.circle(100)
+     t.end_fill()
+draw_star(175,100)
 turtle.done()
-
---
+++ tkinter.py
@@ -0,0 +1,5 @@
+from tkinter import *
+
+tk=TK()
+canvas=Canvas(tk ,width=500,hieght=500)
+canvas.pack()
